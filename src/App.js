51d5f8af5812df7b320e4de015e5c3f098b01a18
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-08-05 13:53:11 +0530

    Boiler plate created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+import About from './pages/About/About';
+import ContactUs from './pages/ContactUs/ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <div className="App">
+              <Navbar />
+              <Home />
+              <Footer />
+            </div>
+          }
+        />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contactUs" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
