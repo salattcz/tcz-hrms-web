17a19cd46c4ba1a9deeb4d21a987127ba396b7c9
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-09-05 14:03:50 +0530

    Link added to table row

--- src/components/Employees/Employees.js
+++ src/components/Employees/Employees.js
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-=======
 import React, { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { getAllUsersApi } from '../../helpers/UsersAPIs';
->>>>>>> 9226598d85371cc427b2cdd160d796883f667e68
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Adminfooter from '../Adminfooter/adminfooter';
 import Adminhomepage from '../Adminhomepage/adminhome';
 import './Employees.css';
+import { getAllUsersApi } from '../../helpers/UsersAPIs';
 import logo from './logo.jpg';
-import Ham from '../Ham/button1';
+// import Ham from '../Ham/button1';
 function Employees() {
+  const navigate = useNavigate();
+
   const [userList, setUserList] = useState([]);
   useEffect(() => {
     getAllUsersApi(0, 3).then((res) => {
@@ -19,6 +16,9 @@ function Employees() {
     });
   }, []);
 
+  const handleClick = () => {
+    navigate('/button1');
+  };
   return (
     <div className="App container">
       <Routes>
@@ -54,60 +54,20 @@ function Employees() {
                     <th scope="col">Reporting Manager</th>
                   </tr>
                 </thead>
-<<<<<<< HEAD
                 <tbody>
-                  <Link className="table-row" to="/button1">
-                    <tr>
-                      <th scope="row">
-                        <img className="pic" src={logo} alt="this is logo image" /> 1
-                      </th>
-
-                      <td>Mahak Agrawal</td>
-
-                      <td>MERN</td>
-                      <td>Saloni Sikdar</td>
-                    </tr>
-                  </Link>
-                  <Routes>
-                    <Route path="/button1" component={<Ham />}></Route>
-                  </Routes>
-
-=======
-                {userList.map((user) => (
-                  <tbody key={user._id}>
-                    <tr>
-                      <th scope="row">{user._id}</th>
-                      <td>
-                        <a href="#">{user.name}</a>
-                      </td>
-                      <td>{user.department}</td>
-                      <td>{user.reportingManager ? user.reportingManager : '---'}</td>
-                    </tr>
-                  </tbody>
-                ))}
-                {/* <tbody>
->>>>>>> 9226598d85371cc427b2cdd160d796883f667e68
-                  <tr>
-                    <th scope="row">
-                      <img className="pic" src={logo} alt="this is logo image" /> 2
-                    </th>
-                    <td>
-                      <a href="#">Lakshay Meghlan</a>
-                    </td>
-                    <td>MERN</td>
-                    <td>Saloni Sikdar</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">
-                      <img className="pic" src={logo} alt="this is logo image" /> 3
-                    </th>
-                    <td>
-                      <a href="#">Nishant Kumar</a>
-                    </td>
-                    <td>Creative</td>
-                    <td>Saloni Sikdar</td>
-                  </tr>
-                </tbody> */}
+                  {userList === []
+                    ? ''
+                    : userList.map((user) => (
+                        <tr onClick={handleClick} key={user._id} style={{ cursor: 'pointer' }}>
+                          <th scope="row">
+                            <img className="pic" src={logo} alt="this is logo image" /> 1
+                          </th>
+                          <td>{user.name}</td>
+                          <td>{user.department}</td>
+                          <td>{user.reportingManager ? user.reportingManager : '---'}</td>
+                        </tr>
+                      ))}
+                </tbody>
               </table>
               <Adminfooter />
             </div>
